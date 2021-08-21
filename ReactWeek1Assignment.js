1b6d2fec3f83d63936bccf2fe98a3307d038010b
jennifertimbol <jennifertmbl@gmail.com>
2021-08-21 13:01:13 -0700

    Updated method

--- ReactWeek1Assignment.js
+++ ReactWeek1Assignment.js
@@ -14,11 +14,31 @@ class Bootcamp{
     }
     registerStudent(studentToRegister) {
         let allStudents = this.students.filter(student => student.email === studentToRegister.email);
-        console.log("all students for add",allStudents)
         if(allStudents.length === 0) {
             this.students.push(studentToRegister);
+            console.log(`Registering ${studentToRegister.email} to the bootcamp Web Dev Fundamentals.`);
+            return this.students;
         }
-        console.log(`Registering ${studentToRegister.email} to the bootcamp Web Dev Fundamentals.`);
-        return this.students;
+        else {
+            console.log(`The student ${studentToRegister.email} is already registered.`)
+        }
+    }
+}
+
+class Bootcamp{
+    constructor(name, level, students = []){
+        this.name = name;
+        this.level = level;
+        this.students = students;
+    }
+    registerStudent(student) {
+        if(this.students.filter(s => s.email === student.email).length) {
+            console.log(`The student ${student.email} is already registered.`)
+        }
+        else {
+            this.students.push(student);
+            console.log(`Registering ${student.email} to the Bootcamp ${this.name}.`)
+        }
+        return students;
     }
 }
